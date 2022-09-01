7966850c03bb289d292be3dadacd6f5bebf40556
tommaso <tommaso.brichetti@outlook.it>
2022-09-01 17:42:48 +0200

    fix

--- esw1d4/src/components/AddComment.jsx
+++ esw1d4/src/components/AddComment.jsx
@@ -1,65 +1,70 @@
-import { Component } from "react";
-import { Col, Container, Row, Form, Button } from "react-bootstrap";
+import { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class AddComment extends Component {
 
     state = {
-        comment: {
-            rate: 5,
-            text: '',
-            elementId: this.props.asin
-        }
+            comment: "",
+            rate: "5",
+            elementId: this.props.asin 
     }
 
-    fetchCommentsPush = async (e) => {
-        e.preventDefault()
-        try {
-            let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/",  {
-                method: 'POST',
-                headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwYjRkMjVlODE3MTAwMTU1YjM1M2IiLCJpYXQiOjE2NjIwMzkyNTAsImV4cCI6MTY2MzI0ODg1MH0.KxUdxQj1M_ncn-UyXnGwGhNU3o6UaXGHNlrAEH8Kocg"
-                },
-                body: JSON.stringify(this.state.comment)
-            })
-
-            if (response.ok) {
-                console.log('nice');
-                this.setState({
-                    comment: {
-                        rate: 5,
-                        text: '',
-                        elementId: ''
-                    }
-                })
-
-            } else {
-               console.log('aiuto piura');
-            }
-        } catch (error) {
-            console.log('something went wrong');
-        }
-    }
-
-    hendleChange = (propretyName, propretyValue) => {
-        this.setState ({
-            comment: {
-                ...this.state.comment,
-                [propretyName]: propretyValue
-            }
+    handleChange = (propertyName, propertyValue) => {
+        this.setState({
+                ...this.state.reservation,
+                [propertyName]: propertyValue,
+            
         })
     }
 
+    addPost = async (e) => {
+
+        
+        e.preventDefault();
+
+        try {
+            let response = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+                method: 'POST',
+                body: JSON.stringify({
+                    comment: this.state.comment,
+                    rate: this.state.rate,
+                    elementId: this.state.elementId
+                }),
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzEwYjUxMTVlODE3MTAwMTU1YjM1M2MiLCJpYXQiOjE2NjIwMzkzMTMsImV4cCI6MTY2MzI0ODkxM30.YFGlcMD6_WlGt6KbQi1b_2mhNfp14Ohl4xdVmVkMJFk",
+                }
+                
+            })
+            if(response.ok) {
+                let data = await response.json();
+                console.log(data)
+                this.setState({
+                        comment: "",
+                        rate: "5",
+                        elementId: ""
+                })
+                
+            } else {
+                console.log('Ocio brody non funge il form')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     render() {
+
         return (
-            <Form onSubmit={this.fetchCommentsPush}>
-                <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form onSubmit={this.addPost}>
+
+                <Form.Group>
                     <Form.Label>Example select</Form.Label>
-                    <Form.Control as="select" 
-                    value={this.state.comment.rate} 
-                    onChange={(e)=>{
-                        this.hendleChange('rate', e.target.value)
-                    }
-                    }>
+                    <Form.Control as="select" value={this.state.rate} 
+                    onChange={(e) => {
+                        this.handleChange('rate', e.target.value)
+                    }}>
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -68,21 +73,22 @@ class AddComment extends Component {
                     </Form.Control>
                 </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Group>
                     <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows={3} 
-                    value={this.state.comment.text} 
-                    onChange={(e)=>{
-                        this.hendleChange('text', e.target.value)
-                    }
-                    }
+                    <Form.Control as="textarea" rows={3} value={this.state.comment} 
+                    onChange={(e) => {
+                        this.handleChange('comment', e.target.value)
+                    }}
                     />
                 </Form.Group>
+                <br />
+                <Button variant="info" type="submit">Submit</Button>
 
-                <Button as="input" type="submit" value="Submit" />
             </Form>
         )
+
     }
+
 }
 
 export default AddComment;
